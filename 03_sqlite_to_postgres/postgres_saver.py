bd7e766338ce34f766ad2bce5c6eee8a4345f595
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-06-11 20:26:42 +0600

    Добавлен метод PostgresSaver._execute_sql()

--- 03_sqlite_to_postgres/postgres_saver.py
+++ 03_sqlite_to_postgres/postgres_saver.py
@@ -19,35 +19,35 @@ class PostgresSaver:
 
     def __init__(self, connection: PGConnection):
         self.connection = connection
+
+    def _execute_sql(self, sql: str, values: tuple):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(sql, values)
+            except UniqueViolation as e:
+                print(e)
+            self.connection.commit()
     
     def save_person(self, person: Person):
-        with self.connection.cursor() as cursor:
-            sql = (
-                'INSERT INTO person(id, full_name, created, modified) '
-                'VALUES (%s, %s, NOW(), NOW());'
-            )
-            values = (person.id, person.full_name)
-            try:
-                cursor.execute(sql, values)
-            except UniqueViolation as e:
-                print(e)
-            self.connection.commit()
+        sql = '''
+            INSERT INTO person (id, full_name, created, modified)
+            VALUES (%s, %s, NOW(), NOW());
+        '''
+        values = (person.id, person.full_name)
+        self._execute_sql(sql, values)
 
     def save_film_work(self, film_work: FilmWork):
-        with self.connection.cursor() as cursor:
-            sql = (
-                'INSERT INTO film_work(id, title, description, rating, type, created, modified) '
-                'VALUES (%s, %s, %s, %s, %s, NOW(), NOW());'
+        sql = '''
+            INSERT INTO film_work (
+                id, title, description, rating, type, created, modified
             )
-            values = (
-                film_work.id,
-                film_work.title,
-                film_work.description,
-                film_work.rating,
-                film_work.type,
-            )
-            try:
-                cursor.execute(sql, values)
-            except UniqueViolation as e:
-                print(e)
-            self.connection.commit()
+            VALUES (%s, %s, %s, %s, %s, NOW(), NOW());
+        '''
+        values = (
+            film_work.id,
+            film_work.title,
+            film_work.description,
+            film_work.rating,
+            film_work.type,
+        )
+        self._execute_sql(sql, values)
