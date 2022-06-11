b095f5a6c7723577806f6268e2a3d029f1865021
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-06-11 19:51:35 +0600

    Добавлен импорт персон в load_data.py

--- 03_sqlite_to_postgres/load_data.py
+++ 03_sqlite_to_postgres/load_data.py
@@ -1,20 +1,33 @@
 import sqlite3
 
 import psycopg2
-from psycopg2.extensions import connection as _connection
+from postgres_saver import PostgresSaver, postgres_connection
+from psycopg2.extensions import connection as PGConnection
 from psycopg2.extras import DictCursor
+from sqlite_loader import SQLiteLoader, sqlite_connection
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
+def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: PGConnection):
+    """Основной метод загрузки данных из SQLite в Postgres."""
 
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
-
+    loader = SQLiteLoader(sqlite_conn)
+    saver = PostgresSaver(pg_conn)
+    with pg_conn.cursor() as curs:
+        curs.execute('TRUNCATE person CASCADE;')
+    pg_conn.commit()
+    for obj in loader.load_persons():
+        saver.save_person(obj.id, obj.full_name)
+        print(obj.id)
 
 if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+    dsl = {
+        'dbname': 'movies_database', 
+        'user': 'app', 
+        'password': '123qwe', 
+        'host': '127.0.0.1', 
+        'port': 5432,
+    }
+    sqlite_db_name = 'db.sqlite'
+    with sqlite_connection(sqlite_db_name) as sqlite_conn:
+        with postgres_connection(dsl) as pg_conn:
+            load_from_sqlite(sqlite_conn, pg_conn)
