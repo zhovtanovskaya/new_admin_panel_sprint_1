8d4faab903cae5386424226d981d4f48bf694eb3
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-06-13 19:34:48 +0600

    Добавлен метод SQLiteLoader.load()
    И удалены методы .load_film_work() и .load_person(),
    которые заменяет .load().

--- 03_sqlite_to_postgres/sqlite_loader.py
+++ 03_sqlite_to_postgres/sqlite_loader.py
@@ -23,7 +23,7 @@ class SQLiteLoader:
     def __init__(self, connection: sqlite3.Connection):
         self.connection = connection
 
-    def _load_data(self, TableDataClass):
+    def load(self, TableDataClass):
         table_name = SOURCE_MAPPING[TableDataClass]
         curs = self.connection.cursor()
         curs.execute('SELECT * FROM {table};'.format(table=table_name))
@@ -32,10 +32,3 @@ class SQLiteLoader:
             row_dict = dict(zip(row.keys(), tuple(row)))
             obj = TableDataClass(**row_dict)
             yield obj
-
-    def load_film_works(self):
-        return self._load_data(FilmWork)
-
-    def load_persons(self):
-        return self._load_data(Person)
-
--- 03_sqlite_to_postgres/tests/test_sqlite_loader.py
+++ 03_sqlite_to_postgres/tests/test_sqlite_loader.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 
+from db_objects import FilmWork, Person
 from sqlite_loader import SQLiteLoader, create_connection
 
 DB_PATH = 'db.sqlite'
@@ -14,12 +15,12 @@ class TestSQLiteLoader(TestCase):
         self.connection.close()
 
     def test_load_film_works(self):
-        data = self.loader.load_film_works()
+        data = self.loader.load(FilmWork)
         record = next(data)
         self.assertEqual('Star Wars: Episode IV - A New Hope', record.title)
 
     def test_load_persons(self):
-        data = self.loader.load_persons()
+        data = self.loader.load(Person)
         record = next(data)
         self.assertEqual('George Lucas', record.full_name)
 
