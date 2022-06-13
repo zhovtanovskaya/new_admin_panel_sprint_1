8feed7db2e6e71a14c6c2f9dc706ab3b2566009d
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-06-13 13:34:40 +0600

    Настройки базы данных для 3 задания перенесены в окружение
    Пример файла с настройками в 03_sqlite_to_postgres/.env.example.

--- 03_sqlite_to_postgres/load_data.py
+++ 03_sqlite_to_postgres/load_data.py
@@ -1,10 +1,14 @@
+import os
 import sqlite3
 
 import psycopg2
+from dotenv import load_dotenv
 from postgres_saver import PostgresSaver, postgres_connection
 from psycopg2.extensions import connection as PGConnection
 from psycopg2.extras import DictCursor
 from sqlite_loader import SQLiteLoader, sqlite_connection
+
+load_dotenv()
 
 
 def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: PGConnection):
@@ -15,19 +19,26 @@ def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: PGConnection):
     with pg_conn.cursor() as curs:
         curs.execute('TRUNCATE person CASCADE;')
     pg_conn.commit()
+
     for obj in loader.load_persons():
-        saver.save_person(obj.id, obj.full_name)
+        saver.save_person(obj)
         print(obj.id)
+        break
+    for obj in loader.load_film_works():
+        saver.save_film_work(obj)
+        print(obj.id)
+        break
+
 
 if __name__ == '__main__':
     dsl = {
-        'dbname': 'movies_database', 
-        'user': 'app', 
-        'password': '123qwe', 
+        'dbname': os.getenv('POSTGRES_DB_NAME'), 
+        'user': os.getenv('POSTGRES_DB_USER'), 
+        'password': os.getenv('POSTGRES_DB_PASSWORD'),
         'host': '127.0.0.1', 
         'port': 5432,
     }
-    sqlite_db_name = 'db.sqlite'
+    sqlite_db_name = os.getenv('SQLITE_DB_NAME')
     with sqlite_connection(sqlite_db_name) as sqlite_conn:
         with postgres_connection(dsl) as pg_conn:
             load_from_sqlite(sqlite_conn, pg_conn)
