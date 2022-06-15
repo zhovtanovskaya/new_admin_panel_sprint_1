d57c70daa95b7333e6ae22fcd12fb7023c790fb1
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-06-15 21:30:24 +0600

    Реализован импорт всех данных из SQLite в PostgreSQL
    С логгированием ошибок.

--- 03_sqlite_to_postgres/load_data.py
+++ 03_sqlite_to_postgres/load_data.py
@@ -1,12 +1,16 @@
 """Основной модуль для импорта кино из SQLite в PostgreSQL."""
 
+import logging
 import sqlite3
 
 import settings
 from db_objects import FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork
 from postgres_saver import PostgresSaver, postgres_connection
+from psycopg2.errors import ForeignKeyViolation
 from psycopg2.extensions import connection as pg_connection
 from sqlite_loader import SQLiteLoader, sqlite_connection
+
+logger = logging.getLogger('root')
 
 
 def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: pg_connection):
@@ -18,18 +22,24 @@ def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: pg_connection):
     """
     loader = SQLiteLoader(sqlite_conn)
     saver = PostgresSaver(pg_conn)
-    with pg_conn.cursor() as curs:
-        curs.execute('TRUNCATE person CASCADE;')
-    pg_conn.commit()
 
-    for obj in loader.load_persons():
-        saver.save_person(obj)
-        print(obj.id)
-        break
-    for obj in loader.load_film_works():
-        saver.save_film_work(obj)
-        print(obj.id)
-        break
+    sources = (FilmWork, Person, PersonFilmWork, Genre, GenreFilmWork)
+    for source in sources:
+        for obj in loader.load(source):
+            try:
+                saver.save(obj)
+            except ForeignKeyViolation as e:
+                logger.error(e)
+            except Exception as e:
+                msg = 'Failed to save {type} id={id}.'.format(
+                    type=type(obj), id=obj.id)
+                logger.error(msg)
+                logger.exception(e)
+            else:
+                msg = 'Imported {type} id={id}.'.format(
+                    type=type(obj), id=obj.id)
+                logger.debug(msg)
+            break
 
 
 if __name__ == '__main__':
